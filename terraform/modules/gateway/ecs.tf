cb6e0ffbc9416eb6d1f2353464bdd94c219beeb8
t <t@host.mozmail.com>
2025-10-19 09:54:05 -0400

    trying empty access_key and secret

--- terraform/modules/gateway/ecs.tf
+++ terraform/modules/gateway/ecs.tf
@@ -34,9 +34,9 @@ resource "aws_ecs_service" "this" {
 
   enable_execute_command = true
 
-  launch_type = "FARGATE"
+  launch_type          = "FARGATE"
   force_new_deployment = true
-  
+
   load_balancer {
     target_group_arn = aws_lb_target_group.this.arn
     container_name   = "gateway"
